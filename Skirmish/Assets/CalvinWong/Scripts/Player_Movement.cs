ae9178f70f9dc8239c2a2baf6871674e5551b67f
STUDENT Calvin C. Wong <Calvin.C.Wong@students.ittralee.ie>
2024-09-18 16:58:24 +0100

    Signed-off-by: STUDENT Calvin C. Wong <Calvin.C.Wong@students.ittralee.ie>

--- Skirmish/Assets/CalvinWong/Scripts/Player_Movement.cs
+++ Skirmish/Assets/CalvinWong/Scripts/Player_Movement.cs
@@ -1,23 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    public float turningSpeed = 45f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0,0,1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3 (1, 1, 1);
+        //if(Input.GetKey(KeyCode.W))
+        //{
+        //    transform.position += new Vector3(0, 0, 1);
+        //}
+        if (shouldMoveForward()) moveForward();
+        if (shouldTurnLeft()) turnLeft();
+        
+    }
 
-        if()
-        {
+    private void turnLeft()
+    {
+        transform.Rotate(new Vector3(0,1,0), turningSpeed * 45 * Time.deltaTime);
+    }
 
-        }
+    private void moveForward()
+    {
+        transform.position += moveSpeed * Vector3.forward * Time.deltaTime;
+    }
+
+    private bool shouldTurnLeft()
+    {
+        return Input.GetKey(KeyCode.A);
+    }
+
+    private bool shouldMoveForward()
+    {
+        //if (Input.GetKey(KeyCode.W)) return true
+        //else return false;
+
+        transform.position += moveSpeed * transform.forward * Time.deltaTime;
+
+        return Input.GetKey(KeyCode.W);
     }
 }
